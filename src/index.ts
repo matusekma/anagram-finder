675832af300d744c9593c74c7c7a85567584b698
Matusek Márton <matusekma@gmail.com>
2022-01-08 11:31:52 +0100

    Implement the CLI app using the functions in anagramOperations

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import readline from "readline"
+import { findAnagrams, createCharMap } from "./anagramOperations";
+import CharMapsByWord from "./types/CharMapsByWord";
+
+var file = './src/assets/wordlist.txt';
+
+const dictionaryCharMaps: CharMapsByWord = []
+
+var fileReader = readline.createInterface({
+    input: fs.createReadStream(file),
+    output: process.stdout,
+    terminal: false
+});
+
+const inputReader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+fileReader.on('line', function (line) {
+    dictionaryCharMaps.push({ word: line, charMap: createCharMap(line) })
+});
+
+function waitForUserInputAndProcess() {
+    inputReader.question("Type a word that you would like to search anagrams for or ':q' if you want to exit in vim style!\n", function (input) {
+        if (input == ":q") {
+            console.log("Bye!")
+            inputReader.close()
+        } else {
+            const anagrams = findAnagrams(createCharMap(input), dictionaryCharMaps)
+            console.log(`The anagrams found for "${input}" are:\n${anagrams.join(", ")}`)
+            waitForUserInputAndProcess()
+        }
+    });
+}
+
+waitForUserInputAndProcess()
